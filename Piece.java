299d451319ecde6e02a7b108f21b9deb51c15279
NikolayBlagoev <62670626+NikolayBlagoev@users.noreply.github.com>
2020-04-10 14:41:15 +0300

    Delete Piece.java

--- Piece.java
+++
@@ -1,9 +0,0 @@
-public abstract class Piece {
-
-   abstract String name();
-   abstract void move(int fromX,int fromY, int toX, int toY);
-   abstract Player player();
-   @Override
-   public abstract String toString();
-
-}
